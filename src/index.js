ae3bc6c07cae98294a3ae9727a3172bc7c543dc6
Josh <programmmerjosh@gmail.com>
2022-04-02 18:07:28 +0100

    Complete styling task from Course Project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+// constants
+var greeting = "Hello";
+const hoursOfDay = new Date().getUTCHours();
+const customStyle = { color: "black" };
+
+// TASK:
+// Show a single h1 that says "Good morning" if between midnight and 12PM.
+// or "Good Afternoon" if between 12PM and 6PM.
+// or "Good evening" if between 6PM and midnight.
+// Dynamically change the color of the h1 using inline css styles.
+// Morning = red, Afternoon = green, Night = blue.
+
+if (hoursOfDay < 12) {
+  customStyle.color = "red";
+  greeting = "Good morning";
+} else if (hoursOfDay >= 12 && hoursOfDay < 18) {
+  customStyle.color = "green";
+  greeting = "Good afternoon";
+} else {
+  customStyle.color = "blue";
+  greeting = "Good evening";
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    {/* Apply the "heading" style in the styles.css */}
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
